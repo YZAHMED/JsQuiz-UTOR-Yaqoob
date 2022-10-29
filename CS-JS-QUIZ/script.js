acf0e198edc59d31f0ba8c22f11af84653f71712
Jacobs <jacobs@Jacobss-MacBook-Pro.local>
2022-10-29 22:14:30 +0400

    JavaScript was added

---
+++ CS-JS-QUIZ/script.js
@@ -0,0 +1,8 @@
+const restartBtn = document.getElementById("restart")
+const prevBtn = document.getElementById("previous")
+const nextBtn = document.getElementById("next")
+const submitBtn = document.getElementById("submit")
+// Choices to be added
+
+const userScore = document.getElementById("user-score")
+const questionText = document.getElementById("total-score") 
